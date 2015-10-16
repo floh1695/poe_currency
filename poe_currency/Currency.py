5b7bfe7edc1642a0925fd6d609ed3f0ea03057dd
Charles Bayley <cbayley95@gmail.com>
2015-10-16 13:15:32 -0700

    Added a few currencies Made it so you can preform basic arithmetic on currencies

--- poe_currency/Currency.py
+++ poe_currency/Currency.py
@@ -8,10 +8,26 @@ class Currency:
         return
 
     def set_value(self, value):
-        self.value = int(value)
+        self.value = float(value)
 
     def get_value(self):
         return self.value
 
-    def __int__(self):
-        return self.get_value()
+    def __float__(self):
+        return float(self.get_value())
+
+    def __add__(self, other):
+        return float(self) + float(other)
+
+    def __sub__(self, other):
+        return float(self) - float(other)
+
+    def __mul__(self, other):
+        return float(self) * float(other)
+
+    def __div__(self, other):
+        return float(self) / float(other)
+
+scroll_fragment = Currency(1,                   'Sf' , 'Scroll Fragment')
+scroll_wisdom   = Currency(scroll_fragment * 5, 'SoW', 'Scroll of Wisdom')
+scroll_portal   = Currency(scroll_wisdom   * 3, 'Ps' , 'Portal Scroll')
